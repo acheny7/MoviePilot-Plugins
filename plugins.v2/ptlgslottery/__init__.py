d830d5eb2a3e21dfa71d8dc03a937b24148cf7e8
G7 <219451537+acheny7@users.noreply.github.com>
2026-09-25 01:01:33 +0800

    Set plugin author display name to G7

--- plugins.v2/ptlgslottery/__init__.py
+++ plugins.v2/ptlgslottery/__init__.py
@@ -26,7 +26,7 @@ class PtlgsLottery(_PluginBase):
     plugin_desc = "复用 MoviePilot 站点 Cookie，自动执行 PTLGS 幸运大转盘。"
     plugin_icon = "Moviepilot_A.png"
     plugin_version = "1.2.0"
-    plugin_author = "gt"
+    plugin_author = "G7"
     author_url = "https://github.com/acheny7"
     plugin_config_prefix = "ptlgslottery_"
     plugin_order = 30
